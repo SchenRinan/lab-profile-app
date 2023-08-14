299fff33396e958be00b897bd082161640d82644
Rinan <jandrewschnell@gmail.com>
2023-08-14 17:52:35 +0200

    Iteration 3 done

--- profile-app-client/src/pages/Login.js
+++ profile-app-client/src/pages/Login.js
@@ -1,6 +1,32 @@
+import { Link } from 'react-router-dom';
+import './Signup.css'
+
 const login = () => {
     return (
-        <div className="Login">hello</div>
+        <div className="Signup">
+            <div className="column">
+                <h2>Log In</h2>
+                <form>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" id="" required />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required />
+                </form>
+                <p>If you don't have an account yet, you can create your account <Link to='/signup'>here</Link>.</p>
+            </div>
+            <div className="column row">
+                <div className="row">
+                    <h3>Hello!!</h3>
+                    <h4>Awesome to have you at IronProfile again!</h4>
+                </div>
+                <div className="row">
+                    <p>
+                        If you signup, you agree with all our terms and conditions where we can do whatever we want with the data!
+                    </p>
+                    <button>Log In</button>
+                </div>
+            </div>
+        </div>
     );
 }
  
--- profile-app-client/src/pages/Signup.js
+++ profile-app-client/src/pages/Signup.js
@@ -1,6 +1,52 @@
+import './Signup.css'
+
 const Signup = () => {
     return (
-        <div className="Signup">signup page</div>
+        <div className="Signup">
+            <div className="column">
+                <h2>Sign Up</h2>
+                <form>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" id="" required />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required />
+                    <label htmlFor="campus">Campus</label>
+                    <select name="campus" id="" required>
+                        <option value=""></option>
+                        <option value="Madrid">Madrid</option>
+                        <option value="Barcelona">Barcelona</option>
+                        <option value="Miami">Miami</option>
+                        <option value="Paris">Paris</option>
+                        <option value="Berlin">Berlin</option>
+                        <option value="Amsterdam">Amsterdam</option>
+                        <option value="México">México</option>
+                        <option value="Sao Paulo">Sao Paulo</option>
+                        <option value="Lisbon">Lisbon</option>
+                        <option value="Remote">Remote</option>
+                    </select>
+                    <label htmlFor="course">Course</label>
+                    <select name="course" id="" required>
+                        <option value=""></option>
+                        <option value="Web Dev">Web Dev</option>
+                        <option value="UX/UI">UX/UI</option>
+                        <option value="Data Analytics">Data Analytics</option>
+                        <option value="Cyber Security">Cyber Security</option>
+                    </select>
+                </form>
+            </div>
+            <div className="column row">
+                <div className="row">
+                    <h3>Hello!!</h3>
+                    <h4>Welcome to IronProfile!</h4>
+                </div>
+                <div className="row">
+                    <p>
+                        If you signup, you agree with all our terms and conditions where we can do whatever we want with the data!
+                    </p>
+                    <button>Create the Account</button>
+                </div>
+            </div>
+        </div>
     );
 }
  
